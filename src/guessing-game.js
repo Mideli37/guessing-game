edfc3d224077185560b4a8e6394a8214dfdd65be
Mideli <muzipovanastia@gmail.com>
2023-08-10 19:23:31 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        this.result = 0
     }
 
     guess() {
-
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min)
+        return this.result
     }
 
     lower() {
-
+        this.max = this.result
     }
 
     greater() {
-
+        this.min = this.result
     }
 }
 
